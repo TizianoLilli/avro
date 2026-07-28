9d75468340f3b873bed61ee0028c27e1548fe064
tiziano <tizianolilli02@gmail.com>
2026-07-28 15:29:57 +0200

    formatting with spotless also integration

--- lang/java/avro/src/test/java/customTests/manual/ParseContextIntegrationTest.java
+++ lang/java/avro/src/test/java/customTests/manual/ParseContextIntegrationTest.java
@@ -1,12 +1,14 @@
 package customTests.manual;
 
-import org.apache.avro.*;
+import org.apache.avro.ParseContext;
+import org.apache.avro.Schema;
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.mockito.*;
+
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.mock;
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class ParseContextIntegrationTest {
@@ -17,7 +19,7 @@ public class ParseContextIntegrationTest {
   private Schema schema;
 
   @Before
-  public void setupTest(){
+  public void setupTest() {
     ctx = new ParseContext();
   }
 
